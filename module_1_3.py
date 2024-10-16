bd3997d09fee1530fa303b06e21a168819dcf535
Ramazanova1979 <Ramazanova1979y@gmail.com>
2024-10-16 10:21:49 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,10 @@
+# Динамическая типизация
+name= 'Яна'
+print('Имя:', name)
+age= 45
+print('Возраст:', age)
+age= age + 1
+print('Новая эра:', age)
+is_student= True
+if type(is_student)==bool:
+    print ('Является студентом:', 'Верно')
